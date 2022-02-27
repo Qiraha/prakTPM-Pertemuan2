02c9a6d273be5815fe00138c8b4dc40b8686ecca
Kelompok7 <123190012@student.upnyk.ac.id>
2022-02-27 16:00:22 +0700

    123190012

---
+++ lib/main.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Login Screen"),
+        ),
+        body: Center(
+          child: Column(
+            children:[
+              Container(
+                  width: 300.0,
+                  child: Image.asset('images/Gekkoukan.png')
+              ),
+              Container(
+                width: 300.0,
+                child: TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(50.0),
+                    ),
+                    hintText: 'Email',
+                  ),
+                ),
+              ),
+              Container(
+                height: 10.0,
+              ),
+              Container(
+                width: 300.0,
+                child: TextField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(50.0),
+                    ),
+                    hintText: 'Password',
+                  ),
+                ),
+              ),
+              Container(
+                height: 20.0,
+              ),
+              Container(
+                width: 300.0,
+                child: ElevatedButton(
+                    onPressed: (){},
+                    child: Text("Log In")
+                ),
+              ),
+              Container(
+                height: 5.0,
+              ),
+              Text("Forget password?")
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
